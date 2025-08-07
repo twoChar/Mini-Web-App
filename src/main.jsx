79891df2bce270390f8bb281f802617634565255
twoChar <tushar20254@iiitd.ac.in>
2025-08-07 12:45:37 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Mini-Web-App">
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 )
